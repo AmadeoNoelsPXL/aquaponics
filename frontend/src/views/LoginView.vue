1180fe34321947f307b37ebe1c74f22073f92d43
AmadeoNoels <noels.amadeo@gmail.com>
2022-10-31 05:33:14 +0100

    create better version of login page

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,44 +1,42 @@
 <template>
-    <b-container fluid style="max-width:fit-content">
-    <b-row>
-      <b-col style="max-width:fit-content">
-        <b-img fluid center src="../assets/aquaponics.jpg" style="max-height: inherit;"></b-img>
-      </b-col>
-      <b-col>
-        <b-row>
-          <div class="d-flex justify-content-start align-middle">
-            <p class="mr-3" ><b>Sign In</b></p>
-            <img src="../assets/ownDesign.png" width="100" height="100">
+  <b-row class="vh-100" align-v="center" align-h="center" >
+    <div class="w-75 h-75" style="background-color:white">
+      <b-row class="w-100 h-100">
+        <b-col class="w-50 row justify-content-end">
+          <img src="../assets/aquaponics.jpg" alt="">
+        </b-col>
+        <b-col class="w-50 d-flex justify-content-center align-items-center p-0">
+          <div class="w-75 h-75 p-0 d-flex justify-content-start" style="">
+            <div class="d-flex flex-column w-75">
+              <div class="row justify-content-start mb-4 w-25 m-0 border-bottom" style="margin: 0px 0px 0px;width:20px">Sign In</div>
+              <form class="mt-4">
+                  <label class="d-flex justify-content-start" for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control mb-4" id="exampleInputEmail1" aria-describedby="emailHelp">
+                
+                
+                  <label class="d-flex justify-content-start" for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1">
+                
+                  <button  type="submit" class="btn btn-primary d-flex justify-content-center w-100 mt-4">Submit</button>
+              </form>
+            <div>
+              
+            </div>
+
+            </div>
+            
           </div>
-          <div>
-            <div style='border-bottom:4px solid #1FBE85'></div>
-          </div>
-        </b-row>
-        <b-row>
-          <b-form>
-            <b-form-row class="mb-2">
-              <b-col>
-                <label for="email" class="justify-content-start" >Email</label>
-                <b-form-input placeholder="johndoe@email.com" type="email">
-                </b-form-input>
-              </b-col>
-            </b-form-row>
-            <b-form-row class="mb-2">
-              <b-col>
-                <label for="password" class="justify-content-start" >Password</label>
-                <b-form-input placeholder="johndoe123" type="password">
-                </b-form-input>
-              </b-col>
-            </b-form-row>
-            <b-form-row>
-              <b-button id="loginbtn" variant="success" class="ml-1 mr-1 w-100" style="background-color:#1FBE85; border-color:#1FBE85">Login</b-button>
-              <b-tooltip target="loginbtn" triggers="hover">
-                  <p>Press here to log in</p>
-                </b-tooltip>
-            </b-form-row>
-          </b-form>
-        </b-row>
-      </b-col>
-    </b-row>
-  </b-container>
+        </b-col>
+      </b-row>  
+    </div>
+  </b-row>
 </template>
+<style>
+body{
+  background-color: lightgrey;
+}
+
+html,body {
+  height: 100%;
+}
+</style>
